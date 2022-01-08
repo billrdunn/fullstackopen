f9af052e222bdf42e2a9fe5fbae49dabf765bbe7
Bill <billrdunn@gmail.com>
2022-01-08 13:02:54 +0000

    Add services/entries.js

---
+++ part2/phonebook/src/services/entries.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/entries'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = (newObject) => {
+    // 'post' sends an object to the server
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    // 'put' replaces the entire note
+    // 'patch' changes some of a note's properties
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+export default {getAll, create, update, remove}
